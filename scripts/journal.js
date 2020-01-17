f75f25ed368d0ebac1fff460720c7ec2d81bc561
Matt Reeds <mattlreeds@gmail.com>
2020-01-17 09:57:38 -0600

    daily journal 2 rough draft

---
+++ scripts/journal.js
@@ -0,0 +1,12 @@
+const journalEntry = {
+    date: "January 16, 2020";
+    concepts: "Objects and for loops";
+    contents: "Learning JavaScript is proving to be a challenge.";
+    mood: "Angry"
+}
+
+const objectsAndLoops = [""];
+
+objectsAndLoops.push(journalEntry);
+
+const 
