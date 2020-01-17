acf4a090b13305188f4f1f6dcb97f1e9dd250603
Matt Reeds <mattlreeds@gmail.com>
2020-01-17 09:58:15 -0600

    daily journal rough draft

--- scripts/journal.js
+++ scripts/journal.js
@@ -9,4 +9,15 @@ const objectsAndLoops = [""];
 
 objectsAndLoops.push(journalEntry);
 
-const 
+const gitFlow = {
+    date: "January 13, 2020";
+    concepts: "Pushing, pulling, and no fly zone";
+    contents: "Everything you need to know about the order of the git work flow.";
+    mood: "confused";
+}
+
+const cssSelecting = {
+    date: "January 8, 2020";
+    concepts: "Selecting by specificity";
+    contents: "Learned how to "
+}
