a63e8b704145cb9212f155fbe159bd69041688f6
Matt Reeds <mattlreeds@gmail.com>
2020-01-21 15:03:23 -0600

    creating function to display html from JS

--- scripts/journal.js
+++ scripts/journal.js
@@ -1,26 +1,51 @@
-const journalEntry = {
-    date: "January 16, 2020";
-    concepts: "Objects and for loops";
-    contents: "Learning JavaScript is proving to be a challenge.";
-    mood: "Angry"
+const journalEntries = [
+    {
+        date: "January 16, 2020",
+        concepts: "Objects and for loops",
+        contents: "Learning JavaScript is proving to be a challenge.",
+        mood: "Angry"
+    },
+    {
+        date: "January 8, 2020",
+        concepts: "CSS specificity",
+        contents: "Selecting elements by their classes",
+        mood: "Happy"
+    },
+    {
+        date: "January 13, 2020",
+        concepts: "Git Workflow",
+        contents: "Everything you need to know about the order of the git work flow.",
+        mood: "confused"
+    },
+    {
+        date: "January 21, 2020",
+        concepts: "API",
+        contents: "Application Programmer Interface",
+        mood: "Sad"
+    }
+
+]
+
+const makeJournalEntryComponent = (journalEntry) => {
+    return `
+        <div>
+            <h1>${journalEntry.concepts}</h1>
+            <section>${journalEntry.contents}</section>
+            <aside>${journalEntry.date}</aside>
+        </div>
+    `
+};
+
+const entryContainer = document.querySelector(".entryLog");
+
+const renderJournalEntries = (entries) => {
+    for (let i = 0; i < entries.length; i++) {
+        const journalEntry = entries[i];
+        entryContainer.innerHTML += makeJournalEntryComponent(
+        journalEntry
+    )
+}
 }
 
-const objectsAndLoops = [];
-
-objectsAndLoops.push(journalEntry);
-
-const gitFlow = {
-    date: "January 13, 2020";
-    concepts: "Pushing, pulling, and no fly zone";
-    contents: "Everything you need to know about the order of the git work flow.";
-    mood: "confused";
-}
-
-const cssSelecting = {
-    date: "January 8, 2020";
-    concepts: "Selecting by specificity";
-    contents: "Learned how to "
-}
-
-gitFlow.push(journalEntry);
-cssSelecting.push(journalEntry);
+renderJournalEntries(journalEntries);
+// it works!!
