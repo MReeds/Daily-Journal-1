5d87fbd3e696b6a3c70a0a8db66b94174d1087ff
Matt Reeds <mattlreeds@gmail.com>
2020-01-23 15:55:22 -0600

    fuctioning modular data

--- scripts/journal.js
+++ scripts/journal.js
@@ -7,7 +7,14 @@ const getEntries = () => {
     })};
 
     getEntries();
-
+    
+    
+    /*
+    Now that you've defined an object whose responsibility is to access the data, 
+    you need to write code in src/scripts/journal.js to use that object and get the data.
+    Once you know you have the data, pass it along to the renderJournalEntries function that 
+    now lives in src/scripts/entriesDom.js. 
+    */
     /*
     Main application logic that uses the functions and objects
     defined in the other JavaScript files.
@@ -15,4 +22,6 @@ const getEntries = () => {
     Change the fake variable names below to what they should be
     to get the data and display it.
 */
-objectWithGetterMethod.methodToGetData().then(functionThatRendersData)
+
+API.getJournalEntries().then(renderJournalEntries)
+// objectWithGetterMethod.methodToGetData().then(functionThatRendersData)
