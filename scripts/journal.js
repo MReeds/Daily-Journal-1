6a7fe9f9d150a9d91b47508dd4a80273a7bbf5ed
Matt Reeds <mattlreeds@gmail.com>
2020-01-31 15:56:00 -0600

    created event listener and conditional

--- scripts/journal.js
+++ scripts/journal.js
@@ -9,19 +9,48 @@ const getEntries = () => {
         renderJournalEntries(entries)
     })};
 
+const button = document.getElementById("record-button");
+
+button.addEventListener("click", () => {
+    // console.log("button has been clicked")
+    const journalDate = document.querySelector("#journalDate").value;
+    const journalConcepts = document.querySelector("#concepts").value;
+    const journalEntry = document.querySelector("#entry").value;
+    const journalMood = document.querySelector("#mood").value
+    console.log(journalDate, journalConcepts, journalEntry, journalMood)
+    
+    if(journalDate && journalConcepts && journalEntry && journalMood){
+console.log("Thanks for filling out your journal!")
+    } else{
+    console.log("Please, fill it out!")
+    }
+})
+
+const journalEntryFactory = (date, concepts, entry, mood) => {
+    return {
+        date: date,
+        concepts: concepts,
+        entry: entry,
+        mood: mood
+    }
+}
+
     getEntries();
 
 API.getJournalEntries().then(renderJournalEntries)
 
+/* When the user clicks the button, 
+you need to create a new entry in your API. 
+The HTTP method that you use to create resources is POST. Guidance on syntax is provided below.
+*/
 
-/*Which of your Javascript modules contains the main logic? 
-In this application, the src/scripts/journal.js is where execution starts. 
-Remove all other script tags from index.html so that it only has a 
-single script tag for journal.js.
+// const button = document.getElementById("search-btn");
 
-The other three modules (data.js, entriesDOM.js and entryComponent.js) 
-have methods that are being used outside the scope of the module. 
-So add export statements to these modules.
+// button.addEventListener("click", () => {
 
-Add import statements at the top the appropriate modules 
-so that each module is importing any module it needs.*/
+
+//   const input = document.getElementById("search-criteria");
+//   const searchCriteria = input.value;
+//   const searchResultPromise = apiManager.searchBusStops(searchCriteria);
+//   searchResultPromise.then(searchResults => {
+//     searchResultsDomManager.renderSearchResults(searchResults);
