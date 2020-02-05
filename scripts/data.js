b2de839fe076a3ad2d1d819cd7c58c24d750c226
Matt Reeds <mattlreeds@gmail.com>
2020-02-05 14:07:19 -0600

    functioning modularization

--- scripts/data.js
+++ scripts/data.js
@@ -1,37 +1,40 @@
-import renderJournalEntries from "./entriesDOM.js"
-
 const baseUrl = "http://localhost:8088";
+/* When the user clicks the button, 
+you need to create a new entry in your API. 
+The HTTP method that you use to create resources is POST. Guidance on syntax is provided below.
+*/
 
-// const getEntries = () => {
-//     const journalAPI = "http://localhost:8088/journalEntries";
-//     fetch(journalAPI)
-//     .then(resp => resp.json())
-//     .then(entries => {
-//         renderJournalEntries(entries)
-//     })};
 
-export default {
-    getEntries() {
+//This Object called APIActions contains two methods that handle fetching data from my json database..
+const apiActions = {
+    getEntries: () => {
         return fetch(`${baseUrl}/journalEntries`)
-            .then(response => response.json())
-            .then(entries => {
-                renderJournalEntries(entries)
-            });
+            .then(response => response.json());
+
     },
-    saveJournalEntry(journalEntryObject) {
+
+    //This method saves journal entries to my jsondatabase
+    saveJournalEntry: journalEntryObject => {
         return fetch(`${baseUrl}/journalEntries`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify(journalEntryObject)
-        }) .then(response => response.json())
+        }).then(response => response.json());
     }
-};
-// const getEntries = () => {
-//     const journalAPI = "http://localhost:8088/journalEntries";
-//     fetch(journalAPI)
-//     .then(resp => resp.json())
-//     .then(entries => {
-//         renderJournalEntries(entries)
-//     })};
+}
+
+//     deleteJournalEntry: (object) => {
+
+//         return fetch("")
+
+//     },
+
+//     updateJournalEntry: (id,object) => {
+
+//         return fetch("")
+//     }
+// };
+
+export default apiActions
--- scripts/journal.js
+++ scripts/journal.js
@@ -1,65 +1,26 @@
-import renderJournalEntries from "./entriesDOM.js";
-import ApiActions from "./data.js";
+import eventManager from "./entriesAddEvent.js"
+import apiActions from "./data.js";
+import domOperations from "./entriesDOM.js";
 
-const getEntries = () => {
-    const journalAPI = "http://localhost:8088/journalEntries";
-    fetch(journalAPI)
-    .then(resp => resp.json())
-    .then(entries => {
-        renderJournalEntries(entries)
-    })};
-
-const button = document.getElementById("record-button");
-
-button.addEventListener("click", () => {
-    // console.log("button has been clicked")
-    const journalDate = document.querySelector("#journalDate").value;
-    const journalConcepts = document.querySelector("#concepts").value;
-    const journalEntry = document.querySelector("#contents").value;
-    const journalMood = document.querySelector("#mood").value
-    // console.log(journalDate, journalConcepts, journalEntry, journalMood)
-    
-    const journalEntryObject = journalEntryFactory(journalDate, journalConcepts, journalEntry, journalMood)
-
-    
-    if(journalDate && journalConcepts && journalEntry && journalMood){
-        console.log("Thanks for filling out your journal!")
-        ApiActions.saveJournalEntry(journalEntryObject).then(getEntries)
-    } else{
-        alert("Please fill out the form.")
-        console.log("Please, fill it out!")
-    };
-})
-
-// saveJournalEntry(journalEntryObject).then(ApiActions.getJournalEntries()).then(renderJournalEntries())
-const journalEntryFactory = (date, concepts, contents, mood) => {
-    return {
-        date: date,
-        concepts: concepts,
-        contents: contents,
-        mood: mood
-    }
-}
+apiActions.getEntries()
+.then(domOperations.renderJournalEntries)
+eventManager.buttonFunction()
 
 
-// const postJournalEntry = (journalEntryObject) => {
-//     fetch("http://localhost:8088/journalEntries", {
-//     method: "POST",
-//     headers: {
-//         "Content-Type": "application/json"
-//     },
-//     body: JSON.stringify(journalEntryObject)
-// })}
 
-getEntries();
-/* When the user clicks the button, 
-you need to create a new entry in your API. 
-The HTTP method that you use to create resources is POST. Guidance on syntax is provided below.
-*/
 
-// const button = document.getElementById("search-btn");
 
-// button.addEventListener("click", () => {
+// post.then(get).then(render)
+
+
+
+
+
+
+
+
+
+
 
 
 //   const input = document.getElementById("search-criteria");
