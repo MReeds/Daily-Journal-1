8b203408e6137882c682c7c4feb19738dd8c5ec0
Matt Reeds <mattlreeds@gmail.com>
2020-02-06 15:57:13 -0600

    functioning delete buttons and filtering radio buttons

--- scripts/entryComponent.js
+++ scripts/entryComponent.js
@@ -2,13 +2,14 @@ const journalCardBuilderObj = {
 
     makeJournalEntryComponent: (journalEntry) => {
         return `
-        <section class="journal-entries">
+        <section class="journal-entries--${journalEntry.id}">
         <div>
         <h1>${journalEntry.concepts}</h1>
         <section>${journalEntry.contents}</section>
         <section>${journalEntry.mood}!</section>
         <aside>${journalEntry.date}</aside>
         </div>
+        <button id="deleteEntry--${journalEntry.id}" class="deleteBtn">Delete</button>
         </section>
         `
     }
@@ -18,3 +19,7 @@ const journalCardBuilderObj = {
 //Make sure to call the object and method because I wrapped this method in an object!!
 
 export default journalCardBuilderObj
+
+/*
+
+*/
--- scripts/journal.js
+++ scripts/journal.js
@@ -1,30 +1,20 @@
 import eventManager from "./entriesAddEvent.js"
 import apiActions from "./data.js";
 import domOperations from "./entriesDOM.js";
+import radioButtonManager from "./buttonEvent.js"
+
 
 apiActions.getEntries()
-.then(domOperations.renderJournalEntries)
-eventManager.buttonFunction()
+    .then(domOperations.renderJournalEntries)
+eventManager.buttonFunction();
+radioButtonManager.radioBtnFunction()
+eventManager.entryDeleteEventListener()
 
 
 
-
-
-// post.then(get).then(render)
-
-
-
-
-
-
-
-
-
-
-
-
-//   const input = document.getElementById("search-criteria");
-//   const searchCriteria = input.value;
-//   const searchResultPromise = apiManager.searchBusStops(searchCriteria);
-//   searchResultPromise.then(searchResults => {
-//     searchResultsDomManager.renderSearchResults(searchResults);
+/*
+Once you have successfully retrieved the value of ok, happy, or sad
+based on which radio button was clicked, you need to filter all of the journal entries. 
+The most straightforward way of doing this is to invoke the getJournalEntries()
+method on your data manager object - which will get all entries - and then use the filter() 
+array method to extract only the entries that have the same mood as the one the user clicked on.*/
